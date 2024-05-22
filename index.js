da35f722673d0ba7633b9d20c7a6707908346c62
hkokoa <vitosrbr@gmail.com>
2024-05-22 15:56:29 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Destiny Z The Beast Server","Bot creator Hoko.xpn"];
+const statusMessages = ["Destiny Z The Best Server ☣ ","Bot creator Hoko.xpn 🔅 "];
 
 
 let currentIndex = 0;
